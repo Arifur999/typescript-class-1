7bca62d55034266d9b7d89c725f5056573596722
Arifur999 <arr96777@gmail.com>
2025-11-09 13:16:44 +0600

    primitive typescript

---
+++ src/primitive.ts
@@ -0,0 +1,22 @@
+//js - ts
+
+//js
+// string,number,boolean,undefined,null
+
+//ts
+//never,unknown,void
+
+let userName:string='arifur'
+
+
+let userId:number =123456
+
+let isAdmin=true;
+isAdmin=false;
+
+let x;
+x=5;
+x='hello'
+
+
+
