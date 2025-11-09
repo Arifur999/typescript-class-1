00a7d28af8f5127715f69bfed286fd5855c7499e
Arifur999 <arr96777@gmail.com>
2025-11-09 13:39:00 +0600

    NonPrimitive array typescript

---
+++ src/nonPrimitive.ts
@@ -0,0 +1,23 @@
+//array,object
+//
+
+
+//string type array 
+
+let list=['eggs','milk','sugar']
+
+//number type array
+
+let price=[10,20,30]
+
+//number | string type array
+
+let listPrice=["eggs",10,"milk",20,"sugar",30]
+
+
+//trople
+
+let coordinates:[number,number]=[20,30]
+let couple:[string,string]=["husband","wife"]
+let nameRoll:[string,number]=["arif",10]
+let destination:[string,string,number]=["dhaka","noakhali",3]
