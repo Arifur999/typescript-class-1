84616e7d7c377af26bac95e15e1a65c0e16292ea
Arifur999 <arr96777@gmail.com>
2025-11-13 21:03:26 +0600

    added keyof type operation constrain

--- src/keyofConstrain.ts
+++ src/keyofConstrain.ts
@@ -10,3 +10,34 @@ cbg:string
 type MyVehicle='bike'| 'car'| 'cng'  // aro vlo bahe lekkha jay..ex
 
 type MyVehicle2=keyof RichVehicle;
+
+
+//------------------------------------------------
+
+
+type User={
+    id: number;
+    name: string;
+    address:{
+        city:string;
+    };
+
+};
+
+const user:User={
+    id:222,
+    name:'arif',
+    address:{
+        city:'dhaka'
+    }
+
+
+
+}
+const getPropertyFromObj=<X>(obj:X,key: keyof X)=>{
+    return obj[key];
+}
+
+const result=getPropertyFromObj(user,'id')
+
+console.log(object);
