145a5484992008080a57b5b0f2ee61b0f1b35989
Arifur999 <arr96777@gmail.com>
2025-11-14 18:48:25 +0600

    added class object

---
+++ src/class.ts
@@ -0,0 +1,19 @@
+//oop class object
+
+ class Animal{
+    name:string;
+    species:string;
+    sound:string
+
+
+    constructor (name:string,species:string,sound:string){
+        this.name=name
+        this.species=species
+        this.sound=sound
+    }
+ }
+
+
+ const dog =new Animal('dogesh',"dog","meow")
+
+ console.log(dog);
