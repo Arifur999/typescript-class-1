ac13cc053c23f5949c449a8d362ce28d52ff70d3
Arifur999 <arr96777@gmail.com>
2025-11-15 23:55:52 +0600

    added encapsulation

---
+++ src/encapsulation.ts
@@ -0,0 +1,28 @@
+class BankAccount {
+  public readonly userId: number;
+  public userName: string;
+//   private userBalance: number;
+  private userBalance: number;    //encapsulation
+
+  constructor(userId: number, userName: string, userBalance: number) {
+    this.userId = userId;
+    this.userName = userName;
+    this.userBalance = userBalance;
+  }
+private addBalance(balance:number){
+    this.userBalance=this.userBalance+balance
+}
+
+callHiddenMethod(balance:number){
+    this.addBalance(balance)
+}
+
+
+}
+
+
+const myAccount =new BankAccount(111,"arif",8880);
+
+myAccount.addBalance(100)
+
+console.log(myAccount);
