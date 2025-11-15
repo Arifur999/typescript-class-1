4277ae4e8c4ac3a8b8d47fc628b0bc21e5118946
Arifur999 <arr96777@gmail.com>
2025-11-15 23:49:19 +0600

    added abstraction abstract class

--- src/abstraction.ts
+++ src/abstraction.ts
@@ -29,6 +29,30 @@ myPlayer.stop()
 
 // abstract class
 
+//idea
+abstract class MediaPlayer1 {
+  abstract play(): void;
+  abstract pause(): void;
+  abstract stop(): void;
+}
 
+//implement
+class MyPlayer1 extends MediaPlayer1 {
+  play() {
+    console.log("play 1 music");
+  }
+  pause() {
+    console.log("pause 1 music");
+  }
+  stop() {
+    console.log("stop 1 music");
+  }
+}
+
+const player = new MyPlayer1();
+
+player.play();
+player.pause();
+player.stop();
 
 
